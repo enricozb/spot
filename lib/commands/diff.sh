ca8ce5529bcb734debc4e9d8c1959dde9289f016
Enrico Borba <enricozb@gmail.com>
2020-07-18 08:10:55 -0700

    ordered diff according to timestamps

--- lib/commands/diff.sh
+++ lib/commands/diff.sh
@@ -21,7 +21,22 @@ spot_diff() {
 
   for spot_file in "${!SPOT_MAP[@]}"; do
     local tracked_file=${SPOT_MAP[$spot_file]}
-    echo "diffing $spot_file"
-    colordiff -r "$spot_file" "$tracked_file"
+
+    # spot_file is the one under $SPOT_FILES
+    # tracked_file is the one in the user's filesystem
+    time_range spot_range "$spot_file"
+    time_range tracked_range "$tracked_file"
+
+    read _ max_s <<< "$spot_range"
+    read _ max_t <<< "$tracked_range"
+
+    # colordiff returns an error code of 1 if diffs exist, and 0 otherwise
+    # because of set -e, any non-zero exit code causes the script to stop
+    # the || : guarantees a 0 exit code
+    if (( $max_s <= $max_t )); then
+      colordiff -r $spot_file $tracked_file || :
+    else
+      colordiff -r $tracked_file $spot_file || :
+    fi
   done
 }
